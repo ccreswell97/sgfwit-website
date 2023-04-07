b19aad24320de1adedb421abdfd6f1fdd09b508e
Carmen Creswell <carmencreswell@gmail.com>
2023-04-06 21:10:13 -0500

    Add more to homepage and css for aboutSection

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,14 @@ function App() {
       <div className='App'>
         <Header/>
         <Box className='main-content'>
-          <Typography fontSize={36} fontWeight={300}>
+          <Typography fontSize={50} fontWeight={300}>
             Elevating, engaging, and empowering local women and gender minorities working in technology.
           </Typography>
         </Box>
-        <Section color={'secondary'} height={'100vh'}/>
+        <Box className='about-section' flexDirection={"column"} sx={{backgroundColor: '#F8DED2', minHeight: '70vh'}}>
+            <Typography fontSize={50} fontWeight={500}>Diversity and inclusivity are essential for innovation and progress in tech.</Typography>
+            <Typography fontSize={36} fontWeight={300}>That's why we are committed to promoting gender diversity and equality in the industry. As a group, we are committed to creating a community-driven experience for our members. We encourage active participation and engagement, and we offer opportunities for members to contribute their expertise and knowledge to our community.</Typography>
+        </Box>
         <Section color={'white'} height={'75vh'}/>
       </div>
   )
